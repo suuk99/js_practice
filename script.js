0658d8ff6e61af6f92e2cf4c22f335322a97bf84
suuk99 <jswook0422@naver.com>
2026-03-15 17:45:05 +0900

    복습 및 설명 추가

--- script.js
+++ script.js
@@ -2,28 +2,29 @@ const input = document.querySelector("#textInput");
 const btn = document.querySelector("#btn");
 const list = document.querySelector("#list");
 const clear = document.querySelector("#clear");
-const cnt = document.querySelector("#cnt"); // 글 개수 -> 글 추가, 삭제 시 업데이트
+const cnt = document.querySelector("#cnt");
 
-let items = []; // 글 저장할 배열
-let editTarget = null;  // 수정할 li 기억할 전역 변수
+let items = [];
+let editTarget = null;
+
+cnt.innerHTML = "글 수 : " + list.children.length;
 
 // 글 추가
-btn.addEventListener("click", function () {
-
+btn.addEventListener("click", function() {
     if (input.value.trim() === "") {
-        alert("입력하세요.");
+        alert("내용을 입력하세요.");
         input.focus();
         return;
     }
 
-    if (editTarget) {
-        const index = [...list.children].indexOf(editTarget);
+    if (editTarget) {   //수정 모드인지 확인
+        const index = [ ...list.children ].indexOf(editTarget); //li가 몇 번째 글인지 찾음
 
-        editTarget.firstChild.textContent = input.value;
-        items[index] = input.value;
+        editTarget.firstChild.textContent = input.value;    //화면 글 수정
+        items[index] = input.value; //배열도 수정
 
-        localStorage.setItem("posts", JSON.stringify(items));
-        editTarget = null;
+        localStorage.setItem("posts", JSON.stringify(items));   //localStorage에 다시 저장
+        editTarget = null;  //수정 모드 종료 -> 그래야 다음에 추가 버튼이 추가 역할을 함
     } else {
         list.innerHTML += `
             <li>
@@ -31,77 +32,78 @@ btn.addEventListener("click", function () {
                 <button onclick="removeList(this)">삭제</button>
                 <button onclick="modifyList(this)">수정</button>
             </li>
-            `;
-        items.push(input.value);    // 사용자가 입력한 글을 배열에 저장
-        localStorage.setItem("posts", JSON.stringify(items));   // 배열을 localStorage에 저장
+        `;
+        items.push(input.value);
+        localStorage.setItem("posts", JSON.stringify(items));
     }
 
     input.value = "";
-    cnt.innerHTML = "전체 글 수 : " + list.children.length;
+    cnt.innerHTML = "글 수 : " + list.children.length;
 });
 
-// 전체 삭제 
-clear.addEventListener("click", function() {
-    if (list.children.length === 0) {   // 현재 글 개수 확인
-        alert("글이 존재하지 않음");
-        return;
-    }
-
-    list.innerHTML = "";    // ul 안의 모든 li 삭제
-    items = [];
-    localStorage.removeItem("posts");
-
-    alert("전체 글 삭제 완료");
-
-    cnt.innerHTML = "전체 글 수 : " + list.children.length;
-});
-
-// 엔터로 글 추가 
-// keypress -> 키보드 눌렀을 때 발생하는 이벤트로 이름 변경 불가능
+// enter로 글 추가
 input.addEventListener("keypress", function(e) {
     if (e.key === "Enter") {
         btn.click();
     }
 });
 
-// 해당 글 삭제 함수
+// 전체 삭제
+clear.addEventListener("click", function() {
+    if (list.children.length === 0) {
+        alert("글이 존재하지 않음");
+        return;
+    }
+
+    list.innerHTML = "";
+    items = [];
+    localStorage.removeItem("posts");
+
+    alert("전체 삭제 완료");
+    cnt.innerHTML = "글 수 : " + list.children.length;
+});
+
+// 삭제
 function removeList(btn) {
-    const li = btn.parentElement;
-    const text = li.firstChild.textContent.trim();
+    const li = btn.parentElement;   //버튼의 부모 요소인 li 찾음
+    const text = li.firstChild.textContent.trim();  //li의 첫번째 자식인 글 내용 찾음
 
-    li.remove();
+    li.remove();    //화면에서 안 보이게 삭제
 
-    items = items.filter(i => i !== text);
-    localStorage.setItem("posts", JSON.stringify(items));
+    items = items.filter(i => i !== text);  //배열에서도 삭제
+    localStorage.setItem("posts", JSON.stringify(items));   //localStorage에 배열 다시 저장
 
-    alert("해당 글 삭제 완료");
-    cnt.innerHTML = "전체 글 수 : " + list.children.length;
+    alert("삭제 완료");
+    cnt.innerHTML = "글 수 : " + list.children.length;
 };
+// 버튼 클릭 -> li 찾기 -> 화면에서 li 삭제 -> items 배열에서도 삭제 -> localStorage에 다시 저장 -> 글 개수 업데이트
 
-// 해당 글 수정 함수
+// 수정 
 function modifyList(btn) {
-    const li = btn.parentElement;
-    const text = li.firstChild.textContent.trim();
+    const li = btn.parentElement;   //버튼의 부모 요소인 li 찾음
+    const text = li.firstChild.textContent.trim();  //li의 첫번째 자식인 글 내용 찾음
 
-    input.value = text;
-    editTarget = li;
+    input.value = text; //input에 text를 넣음
+    editTarget = li;    //수정할 대상을 기억 -> 핵심
 };
+// 버튼 클릭 -> input에 글 내용 넣기/editTarget에 li 저장 -> 사용자가 내용 수정 -> 추가 버튼 클릭->
+// editTarget 존재 확인 -> 해당 li 수정 -> items 배열 수정 -> localStorage에 저장 -> editTaget = null
 
-window.addEventListener("load", function() {
-    const saved = localStorage.getItem("posts");
+// localStorage 저장값 불러오기
+window.addEventListener("load", function () {
+    const saved = this.localStorage.getItem("posts");
 
     if (saved) {
         items = JSON.parse(saved);
         items.forEach(function(text) {
             list.innerHTML += `
-            <li>
-            ${text}
-            <button onclick="removeList(this)">삭제</button>
-            <button onclick="modifyList(this)">수정</button>
-            </li>
+                <li>
+                    ${text}
+                    <button onclick="removeList(this)"> 삭제</button>
+                    <button onclick="modifyList(this)"> 수정</button>
+                </li>
             `;
         });
-
-        cnt.innerHTML = "전체 글 수 : " + list.children.length;
+        cnt.innerHTML = "글 수 : " + list.children.length;
     }
 });
